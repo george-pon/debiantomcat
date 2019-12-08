da8c9a05f9abc8dc285984175654167c794e4fb3
Jun Obama <george@yk.rim.or.jp>
2019-12-08 12:12:48 +0900

    add push image

--- push-image.sh
+++ push-image.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#
-# test run image
-#
-function docker-push-debiantomcat() {
-    docker tag debiantomcat:latest georgesan/debiantomcat:latest
-    docker push georgesan/debiantomcat:latest
-    docker tag debiantomcat:latest georgesan/debiantomcat:debian10-openjdk11-tomcat9
-    docker push georgesan/debiantomcat:debian10-openjdk11-tomcat9
-}
-docker-push-debiantomcat
+
+TAG_LIST=$(awk '/^ENV DEBIANTOMCAT_VERSION/ {print $3;}' Dockerfile)
+IMAGE_NAME=$(awk '/^ENV DEBIANTOMCAT_IMAGE/ {print $3;}' Dockerfile)
+
+REPO_SERV=georgesan/
+
+for i in $TAG_LIST
+do
+    echo $SUDO_DOCKER docker tag ${IMAGE_NAME}:$i ${REPO_SERV}${IMAGE_NAME}:$i
+    $SUDO_DOCKER docker tag ${IMAGE_NAME}:$i ${REPO_SERV}${IMAGE_NAME}:$i
+    echo $SUDO_DOCKER docker push ${REPO_SERV}${IMAGE_NAME}:$i
+    $SUDO_DOCKER docker push ${REPO_SERV}${IMAGE_NAME}:$i
+done
+
