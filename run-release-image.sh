376716287c7ce87e8578daa53978aa487df42054
Jun Obama <george@yk.rim.or.jp>
2019-12-09 14:35:11 +0900

    modify shell

--- run-release-image.sh
+++ run-release-image.sh
@@ -2,10 +2,7 @@
 #
 # test run image
 #
-function docker-run-debiantomcat() {
-    docker pull georgesan/debiantomcat:latest
-    ${WINPTY_CMD} docker run -i -t --rm \
-        -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
-        georgesan/debiantomcat:latest
-}
-docker-run-debiantomcat
+docker pull georgesan/debiantomcat:latest
+${WINPTY_CMD} docker run -i -t --rm \
+    -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
+    georgesan/debiantomcat:latest
