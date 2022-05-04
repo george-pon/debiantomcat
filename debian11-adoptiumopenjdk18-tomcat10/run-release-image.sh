dc147cdf1f9f42a3839caadf361df96d19aba63a
Jun Obama <george@yk.rim.or.jp>
2022-05-04 13:38:22 +0900

    mod image name

--- debian11-adoptiumopenjdk18-tomcat10/run-release-image.sh
+++ debian11-adoptiumopenjdk18-tomcat10/run-release-image.sh
@@ -2,7 +2,7 @@
 #
 # test run image
 #
-docker pull georgesan/debiantomcat:latest
+docker pull georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
 ${WINPTY_CMD} docker run -i -t --rm \
     -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
-    georgesan/debiantomcat:latest
+    georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
