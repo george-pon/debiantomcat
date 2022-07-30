bef6f92cffc44a7e8aaeded66b7cdcdbf11e36e7
Jun Obama <george@yk.rim.or.jp>
2022-07-30 14:48:17 +0900

    fix

--- debian11-adoptiumopenjdk18-tomcat10/run-release-image.sh
+++ debian11-adoptiumopenjdk18-tomcat10/run-release-image.sh
@@ -2,7 +2,7 @@
 #
 # test run image
 #
-docker pull georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
+docker pull docker.io/georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
 ${WINPTY_CMD} docker run -i -t --rm \
     -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
-    georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
+    docker.io/georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
