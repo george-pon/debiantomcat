402d3d62efc7edfd66dcd36968bcd64fdf0d42c9
Jun Obama <george@yk.rim.or.jp>
2022-10-01 16:44:39 +0900

    add openjdk 19

---
+++ debian11-adoptiumopenjdk19-tomcat10/run-release-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# test run image
+#
+docker pull docker.io/georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
+${WINPTY_CMD} docker run -i -t --rm \
+    -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
+    docker.io/georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
