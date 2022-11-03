991479d60a0ef7a31a65ba7ffd68062e96d0b96f
Jun Obama <george@yk.rim.or.jp>
2022-11-03 10:31:47 +0900

    update shell

--- debian11-adoptiumopenjdk19-tomcat10/run-release-image.sh
+++ debian11-adoptiumopenjdk19-tomcat10/run-release-image.sh
@@ -5,4 +5,4 @@
 docker pull docker.io/georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
 ${WINPTY_CMD} docker run -i -t --rm \
     -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
-    docker.io/georgesan/debiantomcat:debian11-adoptiumopenjdk18-tomcat10
+    docker.io/georgesan/debiantomcat:debian11-adoptiumopenjdk19-tomcat10
