01e0b8651566dc22b343cf3abc738df0961ac117
Jun Obama <george@yk.rim.or.jp>
2023-05-01 22:19:44 +0900

    update jdk 20

--- debian11-temurinopenjdk20-tomcat10.1/run-release-image.sh
+++ debian11-temurinopenjdk20-tomcat10.1/run-release-image.sh
@@ -2,7 +2,7 @@
 #
 # test run image
 #
-docker pull docker.io/georgesan/debiantomcat:debian11-temurinopenjdk19-tomcat10.1
+docker pull docker.io/georgesan/debiantomcat:debian11-temurinopenjdk20-tomcat10.1
 ${WINPTY_CMD} docker run -i -t --rm \
     -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
     docker.io/georgesan/debiantomcat:debian11-temurinopenjdk20-tomcat10.1
